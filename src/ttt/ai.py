be943f4d55c7931acb8e7f0087fab32069b5d9d4
Nelson Rengifo <nrengifo3107@gmail.com>
2025-09-10 20:28:47 -0400

    Update ai.py

--- src/ttt/ai.py
+++ src/ttt/ai.py
@@ -1,3 +1,3 @@
 # This picks the moves for AI based on difficulty.
 # returns a choice (row, col), engine checks it.
-# Main idea: returns a legal (row, col).
+# Main idea: returns updated game_state with the AI move.
