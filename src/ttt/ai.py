f5a972ec1ef3db0cbe8354b2cb10e38c8f81e95f
Nelson Rengifo <nrengifo3107@gmail.com>
2025-09-13 18:07:50 -0400

    Delete src/ttt/ai.py
    will keep ai logic inside engine.py for simplicity.

--- src/ttt/ai.py
+++
@@ -1,3 +0,0 @@
-# This picks the moves for AI based on difficulty.
-# check the choice (row, col), engine checks it before adding to game_state.
-# Main idea: returns updated game_state with the AI move.
